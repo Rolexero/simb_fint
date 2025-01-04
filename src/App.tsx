886eb6ac0096ff7f72cce3c0d39a364a22bd0c52
Rotimi <rotimiolaolu@gmail.com>
2025-01-04 03:35:15 +0100

    perf: :construction: add dserver side state management
    add dserver side state management

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import LoadingView from "./components/LoadingView";
 import { ThemeProvider } from "./providers/ThemeProviders";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./service/OueryClient/queryClient";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Suspense fallback={<LoadingView screen />}>
-        <RouterProvider router={router} />
-      </Suspense>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={<LoadingView screen />}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
